9722e47566524a091ddd7cb898f4b84c1f13412e
Igor Guastalla de Lima <limaguastallaigor@gmail.com>
2018-05-02 23:55:11 -0300

    Scrutinizer correction.

--- src/Config/phpbattlerite.php
+++ src/Config/phpbattlerite.php
@@ -1,12 +1,6 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    |
-    |--------------------------------------------------------------------------
-    |
-    */
     'providers' => [
         guastallaigor\PhpBattlerite\ServiceProviders\PhpBattleriteServiceProvider::class,
     ],
