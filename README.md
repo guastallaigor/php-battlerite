8e2b0ed2da5c52d416fbc19dc6d89a3f0531d326
Igor Guastalla de Lima <limaguastallaigor@gmail.com>
2018-05-02 22:37:08 -0300

    Added a TODO list on README. Added docblock on TestCase.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # PHP-battlerite
 
 ## Coming soon...
+
+### TODO
+
+* [ ] Add all the requests available at [Developer Battlerite](https://developer.battlerite.com).
+* [ ] Test on Laravel 5.5+.
+* [ ] Make a README.md.
+* [ ] Add Scrutinizer CI.
+* [ ] Add Travis CI.
+* [ ] Publish package on packagist.
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,11 +5,16 @@ namespace guastallaigor\PhpBattlerite\Tests;
 use Helmich\JsonAssert\JsonAssertions;
 use PHPUnit\Framework\TestCase as PHPUnit;
 
-
 /**
- * Class TestCase
+ * PHP-Battlerite easy API
  *
- * @author  Igor Guastalla de Lima  <limaguastallaigor@gmail.com>
+ * @category  Games
+ * @package   tests
+ * TestCase class
+ * @author    Igor Guastalla de Lima  <limaguastallaigor@gmail.com>
+ * @copyright 2018 PHP Battlerite
+ * @license   MIT https://github.com/guastallaigor/php-battlerite/blob/master/LICENSE
+ * @link      https://github.com/guastallaigor/php-battlerite
  */
 abstract class TestCase extends PHPUnit
 {
